7b028b508016ea425afa0b494ed775e1cf0a941f
Enny <46939885+EnnyGit@users.noreply.github.com>
2021-05-31 00:25:27 +0200

    Mockup statistics page finished

--- app-prototype/lib/pages/statistics.dart
+++ app-prototype/lib/pages/statistics.dart
@@ -32,8 +32,31 @@ class _StatisticsState extends State<Statistics> {
                 width: 200,
                 child: Card(
                   elevation: 5,
-                  child: Text(
-                    'AVG. RESTING HEART RATE'
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                        child: Text(
+                          'AVG. RHR',
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 16
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                        child: Text(
+                          '64',
+                          style: TextStyle(
+                            color: Color(0xFFFF4242),
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold 
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)
@@ -46,8 +69,31 @@ class _StatisticsState extends State<Statistics> {
                 width: 200,
                 child: Card(
                   elevation: 5,
-                  child: Text(
-                    'AVG. PACE'
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                        child: Text(
+                          'AVG. PACE (/km)',
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 16
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                        child: Text(
+                          '8.21',
+                          style: TextStyle(
+                            color: Color(0xFF645DD7),
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold 
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)
@@ -65,12 +111,35 @@ class _StatisticsState extends State<Statistics> {
                 width: 200,
                 child: Card(
                   elevation: 5,
-                  child: Text(
-                    'TOTAL DISTANCE'
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                        child: Text(
+                          'TOTAL DISTANCE',
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 16
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 15, 0, 0),
+                        child: Text(
+                          '24,321 m',
+                          style: TextStyle(
+                            color: Color(0xFF69DC9E),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold 
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)
-                  ),                 
+                  ),
                 ),
               ),
               Container(
@@ -79,8 +148,31 @@ class _StatisticsState extends State<Statistics> {
                 width: 200,
                 child: Card(
                   elevation: 5,
-                  child: Text(
-                    'TIME SPENT RUNNING'
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                        child: Text(
+                          'TIME RUNNING',
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontSize: 16
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 15, 0, 0),
+                        child: Text(
+                          '7.5h',
+                          style: TextStyle(
+                            color: Color(0xFFFF9F1C),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold 
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10)
@@ -89,23 +181,23 @@ class _StatisticsState extends State<Statistics> {
               ),
             ],
           ),
-          Container(
-            padding: EdgeInsets.fromLTRB(25, 10, 25, 20),
-            height: 300,
-            width: 500,
-            child: Card(
-              semanticContainer: true,
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              child: Image(
-                image: AssetImage('assets/graph.png'),
-                fit: BoxFit.fill,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10)
-              ),
-              elevation: 5,         
-            ),
-          ),
+          // Container(
+          //   padding: EdgeInsets.fromLTRB(25, 10, 25, 20),
+          //   height: 300,
+          //   width: 500,
+          //   child: Card(
+          //     semanticContainer: true,
+          //     clipBehavior: Clip.antiAliasWithSaveLayer,
+          //     child: Image(
+          //       image: AssetImage('assets/graph.png'),
+          //       fit: BoxFit.fill,
+          //     ),
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(10)
+          //     ),
+          //     elevation: 5,         
+          //   ),
+          // ),
         ],
       )
     );
