46c3e7b82ef90ec5844ed5a9b0e5c02fbc39ede2
robinfredriks <57940911+robinfredriks@users.noreply.github.com>
2021-06-04 11:53:45 +0200

    Adds training score goal page

--- app-prototype/lib/pages/singleworkouts.dart
+++ app-prototype/lib/pages/singleworkouts.dart
@@ -3,117 +3,111 @@ import 'package:ninja_id_project/pages/workoutpage.dart';
 import 'package:ninja_id_project/services/training.dart';
 
 class SingleWorkouts extends StatelessWidget {
-
   List<Training> trainings = [
-      Training(name: "Steady-state jogging",
-      color: Colors.red,
-      duration: '30-45 min(based on stats)', //this needs to be based on stats
-      icon: 'assets/stopwatch.png',
-      image: 'assets/runner3.png',
-      details: '6 to 8 * 1,000m with no recovery jog(based on stats)',
-      description: 'Maintain a consistent speed, level of intensity and work rate during an exercise session. ',
-      speedlvl: 0,
-      endurancelvl: 2,
-      sleeplvl:  1
-      ),
-
-      Training(name: "Toes poes",
-      color: Colors.red,
-      duration: '40 seconds',
-      icon: 'assets/sport.png',
-      image: 'assets/yoga1.png',
-      details: 'stretching the shins and arches of the feet',
-      description: 'To Do Kneel on your mat with your toes curled under. Sit back on your heels (you can place a yoga block or pillow between your heels and glutes). Breathe deeply for 10 counts. Then, point your toes, place your hands on the mat behind you, and lean back as you attempt to lift your knees off the mat. If your knees don’t come far up, don’t worry. You’ll still feel a nice stretch in your shins and arches.',
-      speedlvl: 0,
-      endurancelvl: 0,
-      sleeplvl:  2),
-
-      
-      Training(name: "Interval jogging",
-      color: Colors.yellow,
-      duration: '60 min(based on stats)', //this needs to be based on stats
-      icon: 'assets/trainers.png',
-      image: 'assets/tennis1.png',
-      details: '6 to 8 * 1,000m with with recovery jog(based on stats)',
-      description: 'Alternate between periods of high-intensity exercise and lower-intensity, active or passive recovery.',
-      speedlvl: 0,
-      endurancelvl: 2,
-      sleeplvl:  0
-      ),
-
-      Training(name: "Squats",
-      color: Colors.purple,
-      duration: '20 min', //maybe also based on stats?
-      icon: 'assets/sport.png',
-      image: 'assets/runner3.png',
-      details: '3 sets of 12',
-      description: 'Start standing with feet just wider than hip-width apart, toes pointed slightly out, and hands clasped at chest for balance. Initiate the movement by sending your hips back first, then bend knees to lower down as far as possible while keeping your chest lifted. You should lower down until thighs are at least parallel to floor. Press through heels and engage glutes to return back to the starting position.',
-      speedlvl: 1,
-      endurancelvl: 1,
-      sleeplvl:  1
-      ),
-
-      Training(name: "Cruise Intervals",
-      color: Colors.red,
-      duration: '30-45 min',
-      icon: 'assets/sport.png',
-      image: 'assets/runner3.png',
-      details: '6 to 8 * 1,000m with 200m recovery jog',
-      description: 'Description',
-      speedlvl: 0,
-      endurancelvl: 2,
-      sleeplvl:  0
-      ),
-
-      Training(name: "Tempo Intervals",
-      color: Colors.red,
-      duration: '30 min',
-      icon: 'assets/sport.png',
-      image: 'assets/runner2.png',
-      details: '4 to 5 * 2,000m WITH 400m recovery jog',
-      description: 'Description',
-      speedlvl: 0,
-      endurancelvl: 1,
-      sleeplvl:  0
-      ),
-
-      Training(name: "Yasso 800s",
-      color: Colors.yellow,
-      duration: '20-25 min',
-      icon: 'assets/sport.png',
-      image: 'assets/boxing1.png',
-      details: '8 to 10 * 800m with equal recovery jog',
-      description: 'Description',
-      speedlvl: 2,
-      endurancelvl: 1,
-      sleeplvl:  0
-      ),
-
-      Training(name: "Goal Pace Run",
-      color: Colors.yellow,
-      duration: '60 min',
-      icon: 'assets/sport.png',
-      image: 'assets/tennis1.png',
-      details: '8 to 12 miles at marathon pace',
-      description: 'Description',
-      speedlvl: 0,
-      endurancelvl: 2,
-      sleeplvl:  0
-      ),
-      
-      
-      Training(name: "Quarters",
-      color: Colors.purple,
-      duration: '30 min',
-      icon: 'assets/sport.png',
-      image: 'assets/runner3.png',
-      details: '8 to 10 * 2 mins with 1-min recovery jog',
-      description: 'Description',
-      speedlvl: 1,
-      endurancelvl: 1,
-      sleeplvl:  0
-      )
-    ];
+    Training(
+        name: "Steady-state jogging",
+        color: Colors.red,
+        duration: '30-45 min(based on stats)', //this needs to be based on stats
+        icon: 'assets/heart.png',
+        image: 'assets/runner3.png',
+        details: '6 to 8 * 1,000m with no recovery jog(based on stats)',
+        description:
+            'Maintain a consistent speed, level of intensity and work rate during an exercise session. ',
+        speedlvl: 0,
+        endurancelvl: 2,
+        sleeplvl: 1),
+    Training(
+        name: "Toes poes",
+        color: Colors.red,
+        duration: '40 seconds',
+        icon: 'assets/sport.png',
+        image: 'assets/yoga1.png',
+        details: 'stretching the shins and arches of the feet',
+        description:
+            'To Do Kneel on your mat with your toes curled under. Sit back on your heels (you can place a yoga block or pillow between your heels and glutes). Breathe deeply for 10 counts. Then, point your toes, place your hands on the mat behind you, and lean back as you attempt to lift your knees off the mat. If your knees don’t come far up, don’t worry. You’ll still feel a nice stretch in your shins and arches.',
+        speedlvl: 0,
+        endurancelvl: 0,
+        sleeplvl: 2),
+    Training(
+        name: "Interval jogging",
+        color: Colors.yellow,
+        duration: '60 min(based on stats)', //this needs to be based on stats
+        icon: 'assets/trainers.png',
+        image: 'assets/tennis1.png',
+        details: '6 to 8 * 1,000m with with recovery jog(based on stats)',
+        description:
+            'Alternate between periods of high-intensity exercise and lower-intensity, active or passive recovery.',
+        speedlvl: 0,
+        endurancelvl: 2,
+        sleeplvl: 0),
+    Training(
+        name: "Squats",
+        color: Colors.purple,
+        duration: '20 min', //maybe also based on stats?
+        icon: 'assets/sport.png',
+        image: 'assets/runner3.png',
+        details: '3 sets of 12',
+        description:
+            'Start standing with feet just wider than hip-width apart, toes pointed slightly out, and hands clasped at chest for balance. Initiate the movement by sending your hips back first, then bend knees to lower down as far as possible while keeping your chest lifted. You should lower down until thighs are at least parallel to floor. Press through heels and engage glutes to return back to the starting position.',
+        speedlvl: 1,
+        endurancelvl: 1,
+        sleeplvl: 1),
+    Training(
+        name: "Cruise Intervals",
+        color: Colors.red,
+        duration: '30-45 min',
+        icon: 'assets/sport.png',
+        image: 'assets/runner3.png',
+        details: '6 to 8 * 1,000m with 200m recovery jog',
+        description: 'Description',
+        speedlvl: 0,
+        endurancelvl: 2,
+        sleeplvl: 0),
+    Training(
+        name: "Tempo Intervals",
+        color: Colors.red,
+        duration: '30 min',
+        icon: 'assets/sport.png',
+        image: 'assets/runner2.png',
+        details: '4 to 5 * 2,000m WITH 400m recovery jog',
+        description: 'Description',
+        speedlvl: 0,
+        endurancelvl: 1,
+        sleeplvl: 0),
+    Training(
+        name: "Yasso 800s",
+        color: Colors.yellow,
+        duration: '20-25 min',
+        icon: 'assets/sport.png',
+        image: 'assets/boxing1.png',
+        details: '8 to 10 * 800m with equal recovery jog',
+        description: 'Description',
+        speedlvl: 2,
+        endurancelvl: 1,
+        sleeplvl: 0),
+    Training(
+        name: "Goal Pace Run",
+        color: Colors.yellow,
+        duration: '60 min',
+        icon: 'assets/sport.png',
+        image: 'assets/tennis1.png',
+        details: '8 to 12 miles at marathon pace',
+        description: 'Description',
+        speedlvl: 0,
+        endurancelvl: 2,
+        sleeplvl: 0),
+    Training(
+        name: "Quarters",
+        color: Colors.purple,
+        duration: '30 min',
+        icon: 'assets/sport.png',
+        image: 'assets/runner3.png',
+        details: '8 to 10 * 2 mins with 1-min recovery jog',
+        description: 'Description',
+        speedlvl: 1,
+        endurancelvl: 1,
+        sleeplvl: 0)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -124,12 +118,13 @@ class SingleWorkouts extends StatelessWidget {
             height: 555,
             child: ListView.builder(
               itemCount: trainings.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                   child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30.0),
-                      child: Card(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Card(
                       child: Container(
                         decoration: BoxDecoration(
                           image: DecorationImage(
@@ -139,7 +134,7 @@ class SingleWorkouts extends StatelessWidget {
                             ),
                           ),
                         ),
-                        child: ListTile(        
+                        child: ListTile(
                           leading: CircleAvatar(
                             backgroundImage: AssetImage(trainings[index].icon),
                             radius: 30,
@@ -164,9 +159,11 @@ class SingleWorkouts extends StatelessWidget {
                           isThreeLine: true,
                           onTap: () {
                             Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => WorkoutPage(training: trainings[index],))
-                            );
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => WorkoutPage(
+                                          training: trainings[index],
+                                        )));
                           },
                         ),
                       ),
@@ -183,12 +180,12 @@ class SingleWorkouts extends StatelessWidget {
 }
 
 class BorderAndStrokeText extends StatelessWidget {
-  const BorderAndStrokeText({
-    Key key,
-    @required this.text,
-    @required this.fontSize,
-    @required this.strokeWidth
-  }) : super(key: key);
+  const BorderAndStrokeText(
+      {Key key,
+      @required this.text,
+      @required this.fontSize,
+      @required this.strokeWidth})
+      : super(key: key);
 
   final String text;
   final double fontSize;
@@ -218,8 +215,6 @@ class BorderAndStrokeText extends StatelessWidget {
           ),
         ),
       ],
-    )
-    ;
+    );
   }
-  
 }
