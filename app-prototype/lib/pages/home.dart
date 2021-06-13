93ad74194b00dbcc375562964a5c7d46d0deb23d
Enny <46939885+EnnyGit@users.noreply.github.com>
2021-06-13 13:49:10 +0200

    Fixed recommended training algoritme

--- app-prototype/lib/pages/home.dart
+++ app-prototype/lib/pages/home.dart
@@ -9,7 +9,7 @@ class _HomeState extends State<Home> {
 
   Map data = {};
   int fitnessLevel = 0;
-  List<int> currentGoal;
+  List<int> currentGoal = [1,1];
 
   @override
   Widget build(BuildContext context) {
